85abbf1074fcdbb16b254b9b37d8d85509e6edf7
pivot5 <anshsharma10@yahoo.ca>
2020-10-02 11:42:37 -0400

    Make efficiency changes & add functions

--- tamil_parser.py
+++ tamil_parser.py
@@ -4,7 +4,11 @@ import tamil, csv
 import tamil.utf8 as utf8
 import sys
 
-def get_common_voice_sentences(file_path):
+stats = {'total_sentences_before_processing':0, 'total_sentences_after_processing':0}
+
+
+#Returns the content of a text file
+def get_file_content(file_path):
     text = ""
     with open(sys.argv[1], 'r', encoding = 'utf-8') as file:
         text = file.read()
@@ -13,55 +17,46 @@ def get_common_voice_sentences(file_path):
     text = text.replace('\ufeff', '')
     text = text.replace('\n', '')
 
-    return sent_tokenize(text)
+    return text
 
-def main():
-
-    if len(sys.argv) < 2:
-        print("Please enter a file name.\n")
-        sys.exit(1)  # abort because of error
-
-    sentences = get_common_voice_sentences(sys.argv[1])
-
-    total_sentences = len(sentences)
-    sentences_gt_14 = 0
-    sentences_lt_3 = 0
-
-
+#Returns a list of sentences in order of appearance. Each sentence is a list of its words and punctuation in order of appearance.
+def get_sentences(text):
+    sentences = sent_tokenize(text)
     for i in range(len(sentences) - 1, -1, -1): #Replace each sentence with a list of its words
         sentences[i] = word_tokenize(sentences[i])
+    stats['total_sentences_before_processing'] = len(sentences)
+    return sentences
 
-    for i in range(len(sentences)): #Remove any words with alphabetical characters
-        for j in range(len(sentences[i]) - 1, -1, -1):
-            if sentences[i][j - 1].upper().isupper():
-                sentences[i].pop(j - 1)
-
-    for i in range(len(sentences) - 1, -1, -1): #Remove sentences with a length less than 1
-        if len(sentences[i - 1]) < 1:
-            sentences.pop(i - 1)
-
-    for i in range(len(sentences) - 1, -1, -1): #Remove sentences with a length greater than 14 and less than 3. Only count words that contain Tamil.
+#Performs the following preprocessing of each sentence in sentences:
+#   Removes any words with alphabetical characters
+#   Removes sentences with a length greater than 14 and less than 3 (Only counts words containing Tamil)
+#   Changes numbers (with no surrounding text) to their Tamil translation
+def preprocess(sentences):
+    for i in range(len(sentences) - 1, -1, -1):
         sentence_length = 0
-        for j in range(len(sentences[i - 1])):
+        for j in range(len(sentences[i]) - 1, -1, -1):
+            #Check if this word contains alphabetical characters. If so, remove it
+            if sentences[i][j].upper().isupper():
+                sentences[i].pop(j)
+                continue
+            #Check if this word contains Tamil characters. If so, add it to the sentence_length counter
             has_tamil = 0
-            for char in sentences[i - 1][j]:
+            for char in sentences[i][j]:
                 if 2960 <= ord(char) <= 3055:
                     has_tamil = 1
             if has_tamil == 1:
                 sentence_length += 1
-        if sentence_length > 14 or sentence_length < 3:
-            sentences.pop(i - 1)
-            if sentence_length > 14:
-                sentences_gt_14 += 1
-            if sentence_length < 3:
-                sentences_lt_3 += 1
-
-    for i in range(len(sentences)): #Change numbers to their Tamil translation
-        for j in range(len(sentences[i])):
+            #Check if this word is a number. If so, convert it to its Tamil translation
             if sentences[i][j].isdigit():
                 sentences[i][j] = tamil.numeral.num2tamilstr_american(float(sentences[i][j]))
+        #Delete the sentence based on its length as calculated earlier.
+        if sentence_length > 14 or sentence_length < 3:
+            sentences.pop(i)
+    return sentences
 
-    for i in range(len(sentences)): #Replace each set of words with a string containing the total sentence
+#Replace each set of words with a string containing the total sentence
+def process(sentences):
+    for i in range(len(sentences)):
         line = ""
         for j in range(len(sentences[i])):
             if sentences[i][j] in ",:.?!\';’”" or (j > 0 and sentences[i][j-1] in "‘“"):
@@ -71,45 +66,56 @@ def main():
             if j == len(sentences[i]) - 1:
                 line = line + "\n"
         sentences[i] = line
+    return sentences
 
+#Performs the following postprocessing of each sentence in sentences:
+#   If end brackets are found, find the start of the bracket and remove everything in between
+#   Remove sentences with no Tamil characters
+#   Remove sentences with special characters
+#   Remove sentences containing numbers with surrounding text
+def postprocess(sentences):
     for i in range(len(sentences) - 1, -1, -1): #If end brackets are found, find the start of the bracket and remove everything in between
         end_bracket_location = -1
-        for j in range(len(sentences[i - 1]) - 1, -1, -1):
-            end_bracket_location = sentences[i-1].find(')')
+        for j in range(len(sentences[i]) - 1, -1, -1):
+            end_bracket_location = sentences[i].find(')')
             if end_bracket_location != -1:
                 for k in range(end_bracket_location - 1, -1, -1): #If end bracket found, search for starting bracket
-                    if sentences[i - 1][k] == '(': #If start bracket found, remove everything in between
-                        sentences[i - 1] = sentences[i - 1][:k - 1] + sentences[i - 1][end_bracket_location + 1:]
+                    if sentences[i][k] == '(': #If start bracket found, remove everything in between
+                        sentences[i] = sentences[i][:k - 1] + sentences[i][end_bracket_location + 1:]
                         break
             else: #If no end bracket, break
                 break
-
-    for i in range(len(sentences) - 1, -1, -1): #Remove sentences with no Tamil characters
+        #Remove sentences with no Tamil, special characters, or numbers with surrounding text
         has_tamil = 0
-        for char in sentences[i - 1]:
-            if 2960 <= ord(char) <= 3055:
+        for char in sentences[i]:
+            if not (2960 <= ord(char) <= 3055):
                 has_tamil = 1
-        if has_tamil == 0:
-            sentences.pop(i - 1)
-
-    for i in range(len(sentences) - 1, -1, -1): #Remove sentences with special characters
-        for char in sentences[i - 1]:
-            if char in "|-@*&^%$#_:":
-                sentences.pop(i - 1)
+            if 48 <= ord(char) <= 57 or char in "|-@*&^%$#_:\[\]\{\}":
+                sentences.pop(i)
+                has_tamil = 1
                 break
+        if has_tamil == 0:
+            print(sentences[i])
+            sentences.pop(i)
+    stats['total_sentences_after_processing'] = len(sentences)
+    return sentences
 
-    for i in range(len(sentences) - 1, -1, -1): #Remove sentences containing numbers in a date format (after other numbers have been converted to Tamil)
-        has_num = 0
-        for char in sentences[i - 1]:
-            if 48 <= ord(char) <= 57:
-                has_num = 1
-        if has_num == 1:
-            sentences.pop(i - 1)
+def main():
+
+    if len(sys.argv) < 2:
+        print("Please enter a file name.\n")
+        sys.exit(1)  # abort because of error
+
+    text = get_file_content(sys.argv[1])
+    sentences = get_sentences(text)
+    sentences = preprocess(sentences)
+    sentences = process(sentences)
+    sentences = postprocess(sentences)
 
     with open('out.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([""])
-        writer.writerow(["Of " + str(total_sentences) + " sentences, " + str(sentences_gt_14) + " of them contained over 14 words and were removed. " + str(sentences_lt_3) + " of them contained less than 3 words and were removed."])
+        writer.writerow(["Total # of sentences before processing: ", stats['total_sentences_before_processing'], ". Total # of sentences after processing: ", stats['total_sentences_after_processing']])
         for i in range(len(sentences)):
             writer.writerow([sentences[i]])
 
