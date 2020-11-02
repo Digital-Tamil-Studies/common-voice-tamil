80d6d34a79ad964a4a32e5ffd3197112d180c66e
parathantl <parathanlive123@gmail.com>
2020-11-03 01:06:44 +0530

    Added CSV reader for senence collector script

--- auto_sentence_collection_adder.py
+++ auto_sentence_collection_adder.py
@@ -1,6 +1,10 @@
 from selenium import webdriver # install gecko driver to use in firefox
 from selenium.webdriver.common.keys import Keys
 import time
+import csv
+
+# Place the CSV file Path here
+batchfile = open('createdata.csv', 'rt')
 
 # Declare the e-mail & password of your GMAIL account
 #Note: Please enter verified email address with your mobile number otherwise while login Google will ask for mobile number
@@ -8,55 +12,74 @@ import time
 email_address = ""
 password_for_email = ""
 
+# Seconds to wait for the pages to load
+wait_seconds = 7
+
 #---- Nothing need to be changed---- 
 browser = webdriver.Firefox()
 browser.get('https://commonvoice.mozilla.org/sentence-collector/login')
 
-time.sleep(10)
+time.sleep(wait_seconds)
 
 googleLogin = browser.find_element_by_xpath("/html/body/div[2]/form/fieldset/div[2]/ul/li[3]/button")
 googleLogin.send_keys(Keys.ENTER)
 
-time.sleep(10)
+time.sleep(wait_seconds)
 
 email = browser.find_element_by_id("identifierId")
 email.send_keys(email_address)
 
 emailSelect = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]").click()
 
-time.sleep(10)
+time.sleep(wait_seconds)
 
 password = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")
 password.send_keys(password_for_email)
 
 PasswordSelect = browser.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]").click()
 
-time.sleep(10)
+time.sleep(wait_seconds)
 
 addBtn = browser.find_element_by_xpath("/html/body/div/header/nav/a[3]")
 addBtn.send_keys(Keys.ENTER)
 
-time.sleep(10)
+time.sleep(wait_seconds)
 
-insertText = browser.find_element_by_xpath("//*[@id=\"sentences-input\"]")
-insertText.send_keys('அருள்புரிந்தனை புரிதலுங் களித்துத் தலையினால் நடந்தேன் விடைப்பாகா ')
+reader = csv.reader(batchfile)
+next(reader, None)  # skip the headers
 
-time.sleep(10)
+for row in reader:
 
-sourceInputText = browser.find_element_by_id("source-input")
-sourceInputText.send_keys('திருவாசகம்')
+    insertText = browser.find_element_by_xpath("//*[@id=\"sentences-input\"]")
+    insertText.send_keys(str(row[1]))
 
-time.sleep(10)
+    time.sleep(wait_seconds)
 
-agreeBtn = browser.find_element_by_id("agree").click()
+    sourceInputText = browser.find_element_by_id("source-input")
+    sourceInputText.send_keys(str(row[0]))
 
-time.sleep(10)
+    time.sleep(wait_seconds)
 
-submitBtn2 = browser.find_element_by_xpath("/html/body/div/div/main/form/section[5]/button")
-submitBtn2.send_keys(Keys.ENTER)
+    agreeBtn = browser.find_element_by_id("agree").click()
 
-time.sleep(10)
+    time.sleep(wait_seconds)
 
-confirmBtn = browser.find_element_by_xpath("/html/body/div/div/main/form/section/button")
-confirmBtn.send_keys(Keys.ENTER)
+    try:
 
+        submitBtn2 = browser.find_element_by_xpath("/html/body/div/div/main/form/section[5]/button")
+        submitBtn2.send_keys(Keys.ENTER)
+        time.sleep(wait_seconds)
+
+    except:
+
+        try:
+            submitBtn2 = browser.find_element_by_xpath("/html/body/div/div/main/form/section[6]/button")
+            submitBtn2.send_keys(Keys.ENTER)
+            time.sleep(wait_seconds)
+        except:
+            print("Not found")
+
+    confirmBtn = browser.find_element_by_xpath("/html/body/div/div/main/form/section/button")
+    confirmBtn.send_keys(Keys.ENTER)
+
+    time.sleep(wait_seconds)
